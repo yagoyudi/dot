599d1394e2cd29ade99422d3a2781e8947b3bc88
yago furuta <yagofuruta@protonmail.com>
2024-04-07 09:08:35 -0300

    fix: .bash_completion/alacritty missing and modularizing code

--- install.sh
+++ install.sh
@@ -1,49 +1,99 @@
 #!/usr/bin/env bash
 
-# create directories
+RED="\033[31;1m"
 export XDG_CONFIG_HOME="$HOME/.config"
-mkdir -p "$XDG_CONFIG_HOME"/alacritty/themes
-mkdir -p "$XDG_CONFIG_HOME"/neovim
 
-# neovim
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo apt update
-sudo apt install make gcc ripgrep unzip neovim
-ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME/nvim"
+make_nvim() {
+	[ ! -x "$(which nvim)" ] && install_nvim
+	setup_nvim
+}
 
-# alacritty
-sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
-git clone https://github.com/alacritty/alacritty.git && cd alacritty
-cargo build --release
-sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
-sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-sudo desktop-file-install extra/linux/Alacritty.desktop
-sudo update-desktop-database
-echo "source $(pwd)/extra/completions/alacritty.bash" >>~/.bashrc
-git clone https://github.com/alacritty/alacritty-theme "$XDG_CONFIG_HOME"/alacritty/themes
-ln -sf "$PWD/alacritty.toml" "$XDG_CONFIG_HOME"/alacritty/alacritty.toml
+install_nvim() {
+	echo -e "${RED}Installing Neovim"
+	sudo add-apt-repository ppa:neovim-ppa/unstable -y
+	sudo apt update
+	sudo apt install make gcc ripgrep unzip neovim
+}
 
-# tmux
-sudo apt install tmux
-ln -sf "$PWD/.tmux.conf" "$HOME/.tmux.conf"
+setup_nvim() {
+	echo -e "${RED}Setting up Neovim"
+	mkdir -p "$XDG_CONFIG_HOME"/nvim
+	ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME/nvim"
+}
 
-# bash
-curl -sS https://starship.rs/install.sh | sh
-ln -sf "$PWD/.bashrc" "$HOME"/.bashrc
-ln -sf "$PWD/.inputrc" "$HOME"/.inputrc
+make_alacritty() {
+	[ ! -x "$(which alacritty)" ] && install_alacritty
+	setup_alacritty
+}
 
-# my notes
-mkdir -p $HOME/repos
-git clone git@github.com:yagofuruta/notes.git "$HOME/repos"
-ln -sf "$PWD/n" "$HOME/.local/bin/n"
+install_alacritty() {
+	echo -e "${RED}Installing Alacritty"
+	sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+	git clone https://github.com/alacritty/alacritty.git && cd alacritty
+	cargo build --release
+
+	sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+	sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+	sudo desktop-file-install extra/linux/Alacritty.desktop
+	sudo update-desktop-database
+
+	mkdir -p ~/.bash_completion
+	cp extra/completions/alacritty.bash ~/.bash_completion/alacritty
+}
+
+setup_alacritty() {
+	echo -e "${RED}Setting up Alacritty"
+	mkdir -p "$XDG_CONFIG_HOME"/alacritty/themes
+	git clone https://github.com/alacritty/alacritty-theme "$XDG_CONFIG_HOME"/alacritty/themes
+	ln -sf "$PWD/alacritty.toml" "$XDG_CONFIG_HOME"/alacritty/alacritty.toml
+}
+
+make_tmux() {
+	[ ! -x "$(which tmux)" ] && install_tmux
+	setup_tmux
+}
+
+install_tmux() {
+	echo -e "${RED}Installing Tmux"
+	sudo apt install tmux
+}
+
+setup_tmux() {
+	echo -e "${RED}Setting up Tmux"
+	ln -sf "$PWD/.tmux.conf" "$HOME/.tmux.conf"
+}
+
+make_bash() {
+	setup_bash
+}
+
+setup_bash() {
+	echo -e "${RED}Setting up Bash"
+	curl -sS https://starship.rs/install.sh | sh
+	ln -sf "$PWD/.bashrc" "$HOME"/.bashrc
+	ln -sf "$PWD/.inputrc" "$HOME"/.inputrc
+}
+
+install_my_notes() {
+	mkdir -p $HOME/repos
+	git clone git@github.com:yagofuruta/notes.git "$HOME/repos"
+	mkdir -p $HOME/.local/bin
+	ln -sf "$PWD/n" "$HOME/.local/bin/n"
+}
 
 # obsidian
-wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.12/obsidian_1.5.12_amd64.deb
-sudo dpkg -i obsidian_1.5.12_amd64.deb
+#wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.12/obsidian_1.5.12_amd64.deb
+#sudo dpkg -i obsidian_1.5.12_amd64.deb
 
 # docker
 
-# vagrant
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install vagrant
+install_vagrant() {
+	wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+	echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+	sudo apt update && sudo apt install vagrant
+}
+
+make_nvim
+make_alacritty
+make_tmux
+make_bash
