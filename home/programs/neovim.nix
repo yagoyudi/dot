943d423a9d84b3347a346f8b2db023b15aa7c84b
Yago Furuta <yagofuruta@protonmail.com>
2024-08-31 19:42:56 -0300

    feat: gopls

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -33,7 +33,32 @@ in {
           })
         '';
         plugins = with pkgs.vimPlugins; [
-          vim-nix
+          {
+            plugin = nvim-lspconfig;
+            type = "lua";
+            config = ''
+							local lspconfig = require("lspconfig")
+							lspconfig.gopls.setup({})
+							vim.api.nvim_create_autocmd("BufWritePre", {
+									pattern = "*.go",
+									callback = function()
+										local params = vim.lsp.util.make_range_params()
+										params.context = {only = {"source.organizeImports"}}
+										local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+										for cid, res in pairs(result or {}) do
+											for _, r in pairs(res.result or {}) do
+												if r.edit then
+													local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+													vim.lsp.util.apply_workspace_edit(r.edit, enc)
+												end
+											end
+										end
+										vim.lsp.buf.format({async = false})
+									end
+							})
+							vim.diagnostic.config({signs = false})
+            '';
+          }
           {
             plugin = telescope-nvim;
             type = "lua";
